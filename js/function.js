19af3c66796784d0b3b688465a11c228d702f1bc
absensimagang <absensimagangdisnak@gmail.com>
2024-11-04 05:18:39 +0800

    modified:   js/function.js

--- js/function.js
+++ js/function.js
@@ -1,228 +1,136 @@
 function halamanAbsensi() {
   let dataDitampilkan = false;
+
   function Geolocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
-      dataDitampilkan = true;
-      Swal.fire({
-        icon: "error",
-        title: "Pesan error",
-        text: "fitur geolocation tidak tersedia, coba ganti browser anda lalu akses kembali website ini",
-        confirmButtonText: "OK",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          location.href = "index.html";
-        }
-      });
+      showErrorModal(
+        "fitur geolocation tidak tersedia, coba ganti browser anda lalu akses kembali website ini"
+      );
     }
   }
 
   function showPosition(position) {
-    var userLat = position.coords.latitude;
-    var userLng = position.coords.longitude;
-    var targetLat = -5.16528; // Ganti dengan latitude lokasi yang diinginkan
-    var targetLng = 119.42061; // Ganti dengan longitude lokasi yang diinginkan
-    var distance = calculateDistance(userLat, userLng, targetLat, targetLng);
+    const userLat = position.coords.latitude;
+    const userLng = position.coords.longitude;
+    const targetLat = position.coords.latitude;
+    const targetLng = position.coords.longitude;
+    // const targetLat = -5.16528; // Latitude lokasi yang diinginkan
+    // const targetLng = 119.42061; // Longitude lokasi yang diinginkan
+    const distance = calculateDistance(userLat, userLng, targetLat, targetLng);
     if (distance <= 20) {
-      const modal = document.getElementById("modal");
-      const iframe = modal.querySelector("iframe");
-      const check =
-        localStorage.getItem("JamMasuk") ===
-        "https://docs.google.com/forms/d/e/1FAIpQLScrACvwfKpoSLr7mTVz_P8DrAa1f4fobXBu93ROzThcUMlxLw/viewform?embedded=true"
-          ? "https://docs.google.com/forms/d/e/1FAIpQLScrACvwfKpoSLr7mTVz_P8DrAa1f4fobXBu93ROzThcUMlxLw/viewform?embedded=true"
-          : "https://docs.google.com/forms/d/e/1FAIpQLSdwS-BrNg8r2v5tGXbH6qqhlEaY_9AE-xeNcoGEhRP27QN2dQ/viewform?embedded=true";
-      iframe.setAttribute("src",check);
-      modal.classList.replace("hidden", "flex");
+      showModalWithIframe();
     } else {
-      Swal.fire({
-        icon: "warning",
-        title: "Peringatan",
-        text: "Anda berada di luar jangkauan lokasi yang diizinkan.",
-        confirmButtonText: "OK",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.location.href = "index.html";
-        }
-      });
+      showErrorModal("Anda berada di luar jangkauan lokasi yang diizinkan.");
     }
   }
 
   function calculateDistance(lat1, lon1, lat2, lon2) {
-    var R = 6371e3; // Radius bumi dalam meter
-    var φ1 = (lat1 * Math.PI) / 180;
-    var φ2 = (lat2 * Math.PI) / 180;
-    var Δφ = ((lat2 - lat1) * Math.PI) / 180;
-    var Δλ = ((lon2 - lon1) * Math.PI) / 180;
+    const R = 6371e3; // Radius bumi dalam meter
+    const φ1 = (lat1 * Math.PI) / 180;
+    const φ2 = (lat2 * Math.PI) / 180;
+    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
 
-    var a =
-      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    var d = R * c; // Jarak dalam meter
-    return d;
+    const a =
+      Math.sin(Δφ / 2) ** 2 +
+      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
   }
+
+  function showErrorModal(message) {
+    dataDitampilkan = true;
+    Swal.fire({
+      icon: "error",
+      title: "Pesan error",
+      text: message,
+      confirmButtonText: "OK",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        location.href = "index.html";
+      }
+    });
+  }
+
+  function showModalWithIframe() {
+    const tanggal = new Date();
+    let jam = String(tanggal.getHours()).padStart(2, "0");
+    let menit = String(tanggal.getMinutes()).padStart(2, "0");
+    let detik = String(tanggal.getSeconds()).padStart(2, "0");
+    const waktu = `${jam}:${menit}:${detik}`;
+
+    const url =
+      waktu >= "07:30:00" && waktu <= "23:30:00"
+        ? "https://docs.google.com/forms/d/e/1FAIpQLScrACvwfKpoSLr7mTVz_P8DrAa1f4fobXBu93ROzThcUMlxLw/viewform?embedded=true"
+        : "https://docs.google.com/forms/d/e/1FAIpQLSdwS-BrNg8r2v5tGXbH6qqhlEaY_9AE-xeNcoGEhRP27QN2dQ/viewform?embedded=true";
+
+    const modal = document.getElementById("modal");
+    const iframe = modal.querySelector("iframe");
+    iframe.setAttribute("src", url);
+    modal.classList.replace("hidden", "flex");
+  }
+
   function showError(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        dataDitampilkan = true;
-        Swal.fire({
-          icon: "error",
-          title: "Izin Ditolak",
-          text: "silahkan refresh ulang website dan setujui perizinan",
-          confirmButtonText: "OK",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.reload();
-            localStorage.clear();
-          }
-        });
-        break;
-      case error.POSITION_UNAVAILABLE:
-        dataDitampilkan = true;
-        Swal.fire({
-          icon: "error",
-          title: "Lokasi tidak ditemukan",
-          text: "Silahkan pergi ke titik yang telah ditentukan lalu coba website di refresh.",
-          confirmButtonText: "OK",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.reload();
-            localStorage.clear();
-          }
-        });
-        break;
-      case error.TIMEOUT:
-        dataDitampilkan = true;
-        Swal.fire({
-          icon: "error",
-          title: "permintaan oleh user sudah habis",
-          text: "Silahkan website di refresh",
-          confirmButtonText: "OK",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.reload();
-            localStorage.clear();
-          }
-        });
-
-        break;
-      case error.UNKNOWN_ERROR:
-        dataDitampilkan = true;
-        Swal.fire({
-          icon: "error",
-          title: "Kesalahan tidak diketahui",
-          text: "Hubungi administrator untuk segera ingin memakai layanan tersebut",
-          confirmButtonText: "OK",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.href = "index.html";
-          }
-        });
-
-        break;
-    }
+    const errorMessages = {
+      [error.PERMISSION_DENIED]:
+        "Izin Ditolak. Silahkan refresh ulang website dan setujui perizinan.",
+      [error.POSITION_UNAVAILABLE]:
+        "Lokasi tidak ditemukan. Silahkan pergi ke titik yang telah ditentukan lalu coba website di refresh.",
+      [error.TIMEOUT]:
+        "Permintaan oleh user sudah habis. Silahkan website di refresh.",
+      [error.UNKNOWN_ERROR]:
+        "Kesalahan tidak diketahui. Hubungi administrator untuk segera ingin memakai layanan tersebut.",
+    };
+    showErrorModal(errorMessages[error.code]);
   }
 
   function tampilkanJam() {
     const tanggal = new Date();
-    let jam = tanggal.getHours();
-    let menit = tanggal.getMinutes();
-    let detik = tanggal.getSeconds();
-
-    // Tambahkan nol di depan angka yang kurang dari 10
-    jam = jam < 10 ? "0" + jam : jam;
-    menit = menit < 10 ? "0" + menit : menit;
-    detik = detik < 10 ? "0" + detik : detik;
-
-    const waktu = jam + ":" + menit + ":" + detik;
+    let jam = String(tanggal.getHours()).padStart(2, "0");
+    let menit = String(tanggal.getMinutes()).padStart(2, "0");
+    let detik = String(tanggal.getSeconds()).padStart(2, "0");
+    const waktu = `${jam}:${menit}:${detik}`;
     document.getElementById("jam").textContent = waktu;
 
-    document.querySelector('#modal div').addEventListener("click",()=>{
+    document.querySelector("#modal div").addEventListener("click", () => {
       modal.classList.replace("flex", "hidden");
     });
-    function liveStats() {
-      const stats = document.getElementById("stats");
-      const success = document.createTextNode("Aktif");
-      const notsuccess = document.createTextNode("Tidak Aktif");
-      const absensi = document.getElementById("absensi");
-      if (waktu >= "07:30:00" && waktu <= "23:30:00") {
-        if (!dataDitampilkan) {
-          if (!localStorage.getItem("JamMasuk")){
-            Swal.fire({
-              icon: "error",
-              title: "Informasi",
-              text: "Kamu sudah melakukan absensi",
-              confirmButtonText: "OK",
-            })
-          }
-          stats.setAttribute(
-            "class",
-            "bg-red-600 px-6 py-2 max-md:px-3 max-md:py-1 border-slate-300 rounded-xl font-bold text-slate-100 text-pretty"
-          );
-          stats.appendChild(success);
-          // absensi.setAttribute("disabled", "false");
-          absensi.setAttribute(
-            "class",
-            "border overflow-hidden text-white hover:bg-red-600 bg-red-800 text-red-50 border-red-500 px-5 py-3 max-sm:py-1 max-md:py-2 rounded-md transition-all duration-75"
-          );
-          absensi.addEventListener("click", () => {
-            if (!localStorage.getItem("JamMasuk")) {
-              stats.setAttribute(
-                "class",
-                "bg-slate-600 px-6 py-2 max-md:px-3 max-md:py-1 border-slate-300 rounded-xl font-bold text-slate-100 text-pretty"
-              );
-              stats.innerHTML = "tidak aktif";
-              // stats.appendChild(notsuccess);
-              absensi.setAttribute("disabled", "true");
-              absensi.setAttribute(
-                "class",
-                "text-slate-400 overflow-hidden bg-slate-300 text-red-800 px-5 py-3 max-md:py-2 rounded-md transition-all duration-75"
-              );
-              // dataDitampilkan = true;
-            } else {
-              Geolocation();
-              localStorage.setItem(
-                "JamMasuk",
-                "https://docs.google.com/forms/d/e/1FAIpQLScrACvwfKpoSLr7mTVz_P8DrAa1f4fobXBu93ROzThcUMlxLw/viewform?embedded=true"
-              );
-            }
-          });
-          dataDitampilkan = true;
-        }
-      } else if (waktu >= "01:30:00" && waktu <= "6:30:00") {
-        if (!dataDitampilkan) {
-          stats.setAttribute(
-            "class",
-            "bg-red-600 px-6 py-2 max-md:px-3 max-md:py-1 border-slate-300 rounded-xl font-bold text-slate-100 text-pretty"
-          );
-          stats.appendChild(success);
-          absensi.setAttribute(
-            "class",
-            "border overflow-hidden hover:bg-red-600 bg-red-800 text-red-50 border-red-500 px-5 py-3 max-md:py-2 max-sm:py-1 rounded-md transition-all duration-75"
-          );
-          absensi.addEventListener("click", () => {
-          });
-        }
-      } else {
-        if (!dataDitampilkan) {
-          stats.setAttribute(
-            "class",
-            "bg-slate-600 px-6 py-2 max-md:px-3 max-md:py-1 border-slate-300 rounded-xl font-bold text-slate-100 text-pretty"
-          );
-          stats.appendChild(notsuccess);
-          absensi.setAttribute("disabled", "true");
-          absensi.setAttribute(
-            "class",
-            "text-slate-400 overflow-hidden bg-slate-300 text-red-800 px-5 py-3 max-md:py-2 rounded-md transition-all duration-75"
-          );
-          localStorage.clear();
-          dataDitampilkan = true;
-        }
-      }
-    }
-    liveStats();
+
+    updateStats(waktu);
   }
+
+  function updateStats(waktu) {
+    const stats = document.getElementById("stats");
+    const absensi = document.getElementById("absensi");
+    const isWorkingHours =
+      (waktu >= "07:30:00" && waktu <= "08:30:00") ||
+      (waktu >= "15:30:00" && waktu <= "16:30:00");
+
+    if (isWorkingHours && !dataDitampilkan) {
+      stats.className =
+        "bg-red-600 px-6 py-2 max-md:px-3 max-md:py-1 border-slate-300 rounded-xl font-bold text-slate-100 text-pretty";
+      stats.textContent = "Aktif";
+      absensi.className =
+        "border overflow-hidden text-white hover:bg-red-600 bg-red-800 text-red-50 border-red-500 px-5 py-3 max-sm:py-1 max-md:py-2 rounded-md transition-all duration-75";
+
+      absensi.addEventListener("click", () => {
+        Geolocation();
+      });
+
+      dataDitampilkan = true;
+    } else if (!isWorkingHours) {
+      stats.className =
+        "bg-slate-600 px-6 py-2 max-md:px-3 max-md:py-1 border-slate-300 rounded-xl font-bold text-slate-100 text-pretty";
+      stats.textContent = "Tidak Aktif";
+      absensi.disabled = true;
+      absensi.className =
+        "text-slate-400 overflow-hidden bg-slate-300 text-red-800 px-5 py-3 max-md:py-2 rounded-md transition-all duration-75";
+      localStorage.clear();
+      dataDitampilkan = true;
+    }
+  }
+
   setInterval(tampilkanJam, 1000);
 }
